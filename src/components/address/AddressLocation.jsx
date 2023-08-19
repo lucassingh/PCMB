c92fc1901e9622384fae5cb461b3b61399482283
lucas.singh <lucas.singh@bizitglobal.com>
2023-08-19 12:11:26 -0300

    fix locations

--- src/components/address/AddressLocation.jsx
+++ src/components/address/AddressLocation.jsx
@@ -1,32 +1,19 @@
 import React from 'react'
-import { Col, Row, Image } from 'react-bootstrap';
+import { Col, Row, Card } from 'react-bootstrap';
 import './address.scss';
 
-const AddressLocation = ({ direccion, nombre, email, telefono, img }) => {
+const AddressLocation = ({ telefono }) => {
     return (
         <Row md={12} className="mt-3 mb-3">
-            <Col md={6} className='d-flex justify-content-center flex-column'>
-                <div className='d-flex flex-column'>
-                    <div className='cont-row d-flex mb-4'>
-                        <strong><span className='label'>Dirección: </span></strong>
-                        <span className='text'>{direccion}</span>
-                    </div>
-                    <div className='cont-row d-flex mb-4'>
-                        <strong><span className='label'>Anfitrion: </span></strong>
-                        <span className='text'>{nombre}</span>
-                    </div>
-                    <div className='cont-row d-flex mb-4'>
-                        <strong><span className='label'>E-mail: </span></strong>
-                        <span className='text'>{email}</span>
-                    </div>
-                    <div className='cont-row d-flex mb-4'>
-                        <strong><span className='label'>Teléfono: </span></strong>
-                        <span className='text'>{telefono}</span>
-                    </div>
-                </div>
-            </Col>
-            <Col md={6} className='mt-4 d-flex justify-content-center align-items-center'>
-                <Image src={img} width='50%' />
+            <Col md={12} className='d-flex justify-content-center flex-column'>
+                <Card style={{ width: '100%' }}>
+                    <Card.Body>
+                        <Card.Title>Télefono de contacto:</Card.Title>
+                        <Card.Text style={{fontSize: 30, fontWeight:'bold', color: '#009929'}}>
+                            {telefono}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
             </Col>
         </Row>
     )
--- src/screens/Locations.jsx
+++ src/screens/Locations.jsx
@@ -3,7 +3,6 @@ import { Col, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import '../styles/section.scss';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import img from '../assets/map.png'
 import AddressLocation from '../components/address/AddressLocation';
 
 const Locations = () => {
@@ -21,67 +20,24 @@ const Locations = () => {
                         <Tabs forceRenderTabPanel defaultIndex={1}>
                             <TabList>
                                 <Tab>Buenos Aires</Tab>
-                                <Tab>CABA</Tab>
                                 <Tab>Santa Fe</Tab>
-                                <Tab>Chaco</Tab>
-                                <Tab>Neuqen</Tab>
-                                <Tab>La Pampa</Tab>
                                 <Tab>Mendoza</Tab>
-                                <Tab>Chubut</Tab>
+                                <Tab>San Juan</Tab>
+                                <Tab>Río Negro</Tab>
+                                <Tab>La Rioja</Tab>
+                                <Tab>Stgo. del Estero</Tab>
                             </TabList>
                             <TabPanel>
                                 <Tabs forceRenderTabPanel>
                                     <TabList>
-                                        <Tab>Lugano</Tab>
-                                        <Tab>La Plata</Tab>
-                                        <Tab>Longchamps</Tab>
+                                        <Tab>San Miguel</Tab>
                                         <Tab>Mar del Plata</Tab>
-                                        <Tab>Bahía Blanca</Tab>
-                                        <Tab>Tandil</Tab>
-                                        <Tab>Punta Alta</Tab>
-                                        <Tab>Lanús</Tab>
-                                        <Tab>Campana</Tab>
-                                        <Tab>Gonzalez Catan</Tab>
                                     </TabList>
                                     <TabPanel>
-                                        <AddressLocation direccion="Rivadvia 890" nombre="Carlos Rodriguez" email="crodriguez@pcmb.com" telefono="+54925665458" img={img} />
+                                        <AddressLocation telefono="+54 9 11 2757 9390" />
                                     </TabPanel>
                                     <TabPanel>
-                                        <AddressLocation direccion="Calle Libertad 456" nombre="Juan López" email="juan.lopez@example.com" telefono="+54998765432" img={img} />
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <AddressLocation direccion="Carrera 789" nombre="María González" email="maria.gonzalez@example.com" telefono="+54955555555" img={img} />
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <AddressLocation direccion="Calle 25 de Mayo 987" nombre="Luis Rodríguez" email="luis.rodriguez@example.com" telefono="+54933333333" img={img} />
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <AddressLocation direccion="Avenida Rivadavia 4567" nombre="Elena García" email="elena.garcia@example.com" telefono="+54977777777" img={img} />
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <AddressLocation direccion="Calle San Martín 543" nombre="Carlos Fernández" email="carlos.fernandez@example.com" telefono="+54988888888" img={img} />
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <AddressLocation direccion="Avenida Belgrano 789" nombre="Laura Pérez" email="laura.perez@example.com" telefono="+54966666666" img={img} />
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <AddressLocation direccion="Calle Corrientes 1234" nombre="Javier Ramírez" email="javier.ramirez@example.com" telefono="+54922222222" img={img} />
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <AddressLocation direccion="Avenida Pueyrredón 567" nombre="Marta Silva" email="marta.silva@example.com" telefono="+54911111111" img={img} />
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <AddressLocation direccion="Calle Florida 890" nombre="Andrés Martín" email="andres.martin@example.com" telefono="+54999999999" img={img} />
-                                    </TabPanel>
-                                </Tabs>
-                            </TabPanel>
-                            <TabPanel>
-                                <Tabs forceRenderTabPanel>
-                                    <TabList>
-                                        <Tab>Caballito</Tab>
-                                    </TabList>
-                                    <TabPanel>
-                                        <AddressLocation direccion="Avenida Santa Fe 4321" nombre="Carolina López" email="carolina.lopez@example.com" telefono="+54944444444" img={img} />
+                                        <AddressLocation telefono="+54 9 223 691 1297" />
                                     </TabPanel>
                                 </Tabs>
                             </TabPanel>
@@ -89,67 +45,91 @@ const Locations = () => {
                                 <Tabs forceRenderTabPanel>
                                     <TabList>
                                         <Tab>Rosario</Tab>
-                                        <Tab>Venado Tuerto</Tab>
+                                        <Tab>Gálvez</Tab>
+                                        <Tab>V. Constitución</Tab>
                                     </TabList>
                                     <TabPanel>
-                                        <AddressLocation direccion="Calle Lavalle 654" nombre="Diego González" email="diego.gonzalez@example.com" telefono="+54977777777" img={img} />
+                                        <AddressLocation telefono="+54 9 341 357 2897" />
                                     </TabPanel>
                                     <TabPanel>
-                                        <AddressLocation direccion="Avenida Callao 987" nombre="Florencia Fernández" email="florencia.fernandez@example.com" telefono="+54988888888" img={img} />
+                                        <AddressLocation telefono="+54 9 3404 45 9741" />
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <AddressLocation telefono="+54 9 3400 41 9089" />
                                     </TabPanel>
                                 </Tabs>
                             </TabPanel>
                             <TabPanel>
                                 <Tabs forceRenderTabPanel>
                                     <TabList>
-                                        <Tab>Resistencia</Tab>
-                                        <Tab>Saenz Peña</Tab>
+                                        <Tab>Capital</Tab>
+                                        <Tab>Luján de Cuyo</Tab>
+                                        <Tab>Palmira</Tab>
                                     </TabList>
                                     <TabPanel>
-                                        <AddressLocation direccion="Calle Reconquista 321" nombre="Gustavo Pérez" email="gustavo.perez@example.com" telefono="+54966666666" img={img} />
+                                        <AddressLocation telefono="+54 9 261 630 0707" />
                                     </TabPanel>
                                     <TabPanel>
-                                        <AddressLocation direccion="Avenida Córdoba 765" nombre="Inés Ramírez" email="ines.ramirez@example.com" telefono="+54922222222" img={img} />
+                                        <AddressLocation telefono="+54 9 261 614 6257" />
                                     </TabPanel>
-                                </Tabs>
-                            </TabPanel>
-                            <TabPanel>
-                                <Tabs forceRenderTabPanel>
-                                    <TabList>
-                                        <Tab>Neuqen</Tab>
-                                    </TabList>
                                     <TabPanel>
-                                        <AddressLocation direccion="Calle Sarmiento 234" nombre="Jorge Silva" email="jorge.silva@example.com" telefono="+54911111111" img={img} />
+                                        <AddressLocation telefono="+54 9 2634 21 7848" />
                                     </TabPanel>
                                 </Tabs>
                             </TabPanel>
                             <TabPanel>
                                 <Tabs forceRenderTabPanel>
                                     <TabList>
-                                        <Tab>La Pampa</Tab>
+                                        <Tab>Capital</Tab>
                                     </TabList>
                                     <TabPanel>
-                                        <AddressLocation direccion="Avenida 9 de Julio 543" nombre="Karina Martínez" email="karina.martinez@example.com" telefono="+54999999999" img={img} />
+                                        <AddressLocation telefono="+54 9 264 581 1017" />
                                     </TabPanel>
                                 </Tabs>
                             </TabPanel>
                             <TabPanel>
                                 <Tabs forceRenderTabPanel>
                                     <TabList>
-                                        <Tab>Mendoza</Tab>
+                                        <Tab>Sierra Grande</Tab>
+                                        <Tab>Senillosa</Tab>
+                                        <Tab>Gral. Roca</Tab>
+                                        <Tab>Fernandez Oro</Tab>
+                                        <Tab>C. Cordero</Tab>
                                     </TabList>
                                     <TabPanel>
-                                        <AddressLocation direccion="Calle Esmeralda 876" nombre="Lorenzo López" email="lorenzo.lopez@example.com" telefono="+54944444444" img={img} />
+                                        <AddressLocation telefono="+54 9 2984 51 3793" />
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <AddressLocation telefono="+54 9 299 507 8562" />
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <AddressLocation telefono="+54 9 2984 55 9609" />
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <AddressLocation telefono="+54 9 299 570 2283" />
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <AddressLocation telefono="+54 9 299 471 4509" />
                                     </TabPanel>
                                 </Tabs>
                             </TabPanel>
                             <TabPanel>
                                 <Tabs forceRenderTabPanel>
                                     <TabList>
-                                        <Tab>Comodoro Rivadavia</Tab>
+                                        <Tab>Villa Unión</Tab>
                                     </TabList>
                                     <TabPanel>
-                                        <AddressLocation direccion="Avenida Juan B. Justo 123" nombre="Marina González" email="marina.gonzalez@example.com" telefono="+54977777777" img={img} />
+                                        <AddressLocation telefono="+54 9 3804 50 2027" />
+                                    </TabPanel>
+                                </Tabs>
+                            </TabPanel>
+                            <TabPanel>
+                                <Tabs forceRenderTabPanel>
+                                    <TabList>
+                                        <Tab>Capital</Tab>
+                                    </TabList>
+                                    <TabPanel>
+                                        <AddressLocation telefono="+54 9 3856 98 6098" />
                                     </TabPanel>
                                 </Tabs>
                             </TabPanel>
