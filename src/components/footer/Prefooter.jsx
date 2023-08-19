4ecc248842c2d026e365e97f0f2cfc79b5b21ea8
lucas.singh <lucas.singh@bizitglobal.com>
2023-08-19 18:44:12 -0300

    cambios sn

--- src/components/footer/Prefooter.jsx
+++ src/components/footer/Prefooter.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Col, Row, Image } from 'react-bootstrap';
 import logos from '../../assets/logos.png';
-import './prefooter.scss'
+import './prefooter.scss';
+import img1 from '../../assets/instagram.png'
+import img2 from '../../assets/facebook.png'
 import { useTranslation } from 'react-i18next';
 
 const Prefooter = () => {
@@ -24,6 +26,14 @@ const Prefooter = () => {
                                 + 54 9 11 5979 4129  <br />
                                 infopcmb@gmail.com
                             </span>
+                            <div className='d-flex flex-row mt-3 align-items-center'>
+                                <Image className='right' src={img1} width='12%' />
+                                <span className='text'>infopcmb</span>
+                            </div>
+                            <div className='d-flex flex-row mt-3 align-items-center'>
+                                <Image className='right' src={img2} width='14%' />
+                                <span className='text'>infopcmb</span>
+                            </div>
                         </div>
                     </Col>
                 </Row>
